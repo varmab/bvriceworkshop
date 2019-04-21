cbbb46769afe0d6cdd2f66d298f911dfd6890814
Varma Bhupatiraju <varmab@gmail.com>
2019-04-21 12:00:24 +0530

    added mongodb support

--- hellobvrice/server.js
+++ hellobvrice/server.js
@@ -1,23 +1,39 @@
 var express=require("express");
 var app=express();
 
+var mongoose=require("mongoose");
+mongoose.connect("mongodb://localhost:27017/bvriceworkshop")
+
+var studentSchema=mongoose.Schema({
+    name: String,
+    age: Number
+})
+var Student=mongoose.model('Student',studentSchema,"students");
+
+app.get("/api/students",function(req,res){
+    Student.find({},function(err,students){
+        if(err) res.send(err);
+        res.send(students)
+    })
+})
+
 app.use(express.static("public"));
 
 // app.get("/",function(req,res){
 //     res.sendFile(__dirname + "/public/contact.html")
 // })
 
-app.get("/",function(req,res){
-    res.sendFile(__dirname + "/public/index.html")
-})
+// app.get("/",function(req,res){
+//     res.sendFile(__dirname + "/public/index.html")
+// })
 
-app.get("/contact",function(req,res){
-    res.sendFile(__dirname + "/public/contact.html")
-})
+// app.get("/contact",function(req,res){
+//     res.sendFile(__dirname + "/public/contact.html")
+// })
 
-app.get("/about",function(req,res){
-    res.sendFile(__dirname + "/public/about.html")
-})
+// app.get("/about",function(req,res){
+//     res.sendFile(__dirname + "/public/about.html")
+// })
 
 app.listen(5000,function(){
     console.log("Server is started")
